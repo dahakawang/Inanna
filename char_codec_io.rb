b050d724caae69c43443f8fcd14efc3dc9d227fc
David Wang <DevilDavidWang@gmail.com>
2013-03-04 16:20:25 +0800

    Improved encoding detection

--- char_codec_io.rb
+++ char_codec_io.rb
@@ -6,18 +6,30 @@ module Inanna
   end
 
   class CharCodecIO
-    SUPPORTED_ENCODINGS = %w(
-      GBK
-      UTF-8
-      UTF-16
-    )
+    SUPPORTED_ENCODINGS = {
+      'UTF-8' => "\xEF\xBB\xBF".force_encoding("ASCII-8BIT"),
+      'UTF-16LE' => "\xFF\xFE".force_encoding("ASCII-8BIT"),
+      'UTF-16BE' => "\xFE\xFF".force_encoding("ASCII-8BIT"),
+    }
     
     attr_reader :last_encoding
 
-    def decodeFromFile(file)
-      data = IO.read(file)
+    def initialize
+      @specified_enc = nil
+    end
 
-      raise EncodingNotDetectError, "Cannot detect character encodings" unless guessEncoding(data)
+    def decodeFromFile(file)
+      data = IO.binread(file)
+
+      # guess the encoding
+      guessed = guessEncoding(data)
+      raise EncodingNotDetectError, "Cannot detect character encodings" if guessed == "UNKNOWN"
+
+      # convert to utf8 
+      @last_encoding = guessed
+      data = stripSignature(data, guessed)
+      data.force_encoding(guessed)
+      data.encode!("UTF-8", {:invalid => :replace, :undef => :replace, :replace => '?'})
 
       data
     end
@@ -28,27 +40,35 @@ module Inanna
               :undef => :replace, 
               :replace => '?'})
 
-      IO.write(file, converted, encoding)
+      IO.binwrite(file, converted, encoding)
     end
 
+    def specifyEncoding(encoding)
+      @specified_enc = encoding
+    end
 
     def guessEncoding(str)
-      original = str.encoding
+      return @specified_enc if @specified_enc
 
-      SUPPORTED_ENCODINGS.each do |encoding|
-        str.force_encoding(encoding)
-        if str.valid_encoding?
-          @last_encoding = encoding
-          str.encode!("UTF-8", 
-            {:invalid => :replace, 
-              :undef => :replace, 
-              :replace => '?'})
-          return true
+      str.force_encoding("ASCII-8BIT")
+      SUPPORTED_ENCODINGS.each do |key, value|
+        if str.start_with? value
+          return key
         end
       end
 
-      str.force_encoding(original)
-      return false
+      return "GBK" if str.force_encoding("GBK").valid_encoding?
+
+      "UNKNOWN"
+    end
+
+    def stripSignature(str, encoding)
+      signature = SUPPORTED_ENCODINGS[encoding]
+      if signature
+        return str[(signature.length)..(str.length - 1)]
+      end
+
+      str
     end
   end
 end
